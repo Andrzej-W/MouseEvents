4c1e96b6211abbad970d73323a8488b6926be2b6
Andrzej Włoszczyński <awloszczynski@hps.com.pl>
2021-08-30 14:52:21 +0200

    Działają zdarzenia onpointer, grid źle się wyświetla. Pewnie brakuje definicji wierszy.

---
+++ MouseEvents/Client/Shared/EventHandlers.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace MouseEvents.Client.Shared;
+
+//[EventHandler("onmouseenter", typeof(MouseEventArgs), true, true)]
+//[EventHandler("onmouseleave", typeof(MouseEventArgs), true, true)]
+//public static class EventHandlers
+//{
+//}
